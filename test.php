8b525aaf4e73cd8005e58bfe9c285e49d108fd0e
GLOBALE\hefawei <hefawei@globalegrow.com>
2018-06-27 15:29:44 +0800

    提交测试

--- test.php
+++ test.php
@@ -7,3 +7,18 @@ echo 55555;
 echo 'develop';
 echo 'feature-test';
 echo 'fea-eee';
+
+echo 'cccc';
+echo 'vvvv';
+
+/**
+ * @desc     函数功能简述
+ * @state    功能备注/声明
+ * @param    參數说明
+ * @dateTime 2018-06-22
+ * @author   henry
+ * @return   [type]
+ */
+function test() {
+    
+}
